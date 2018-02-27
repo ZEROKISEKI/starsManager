7bcd2062bcf040f32a82b0283519017c9a45f770
ZEROKISEKI <20142100095@m.scnu.edu.cn>
2018-02-27 21:19:34 +0800

    first commit

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
-package starsManager
+package main
+
+import (
+	"flag"
+	"github.com/asticode/go-astilectron"
+)
+
+var (
+	AppName string
+	debug = flag.Bool("d", false, "enables the debug mode")
+	w       *astilectron.Window
+)
+
+func main() {
+
+}
